3fd4201c208510ed25d9a00013db15deb34339da
Adem <zidadem36@gmail.com>
2026-05-10 00:25:53 +0000

    final fix

--- app.js
+++ app.js
@@ -62,13 +62,6 @@ const CONTRACT_ABI = [
 }
 ];
 
-const STATUS_MAP = [
-  "Created",
-  "In Transit",
-  "Delivered",
-  "Recalled"
-];
-
 const web3 = new Web3(
   "https://sepolia.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161"
 );
